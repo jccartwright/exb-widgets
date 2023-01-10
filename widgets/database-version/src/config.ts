d4ac44081e841df466b92f25cdba267d452c719d
jccartwright <john.c.cartwright@comcast.net>
2023-01-09 18:57:35 -0700

    database-version: read version from external URL

--- widgets/database-version/src/config.ts
+++ widgets/database-version/src/config.ts
@@ -1,7 +1,7 @@
-import { ImmutableObject } from 'seamless-immutable';
+import { ImmutableObject } from 'seamless-immutable'
 
 export interface Config {
-  databaseVersion: string;
+  configUrl: string
 }
 
-export type IMConfig = ImmutableObject<Config>;
+export type IMConfig = ImmutableObject<Config>
